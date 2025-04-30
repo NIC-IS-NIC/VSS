78ce1d6c9d6d76a59dd4985d055647a3801aab18
Joel Hughes <joel@nanopay.net>
2025-04-30 10:51:08 -0400

    code cleanup

--- src/hughes/vss/Make.js
+++ src/hughes/vss/Make.js
@@ -1,13 +1,12 @@
 foam.CLASS({
-    package: 'hughes.vss',
-    name: 'Make',
-    discription: 'brand of car manufacturer',
-    properties: [
-        {
-            name: 'id',
-            class: 'String',
-            label: 'Name'
-        }
-    ]
-
+  package: 'hughes.vss',
+  name: 'Make',
+  discription: 'brand of car manufacturer',
+  properties: [
+    {
+      name: 'id',
+      class: 'String',
+      label: 'Name'
+    }
+  ]
 })
